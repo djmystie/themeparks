92bc74c0c9f2dc7f6b39462302f4b2e6c2917c3a
Chris Blagg <chrisblagg@chriss-mbp.lan>
2021-07-13 23:45:40 -0700

    proxy test

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -4,23 +4,23 @@ import { useQuery } from 'react-query'
 export default function Destinations({setResort}){
     const destinations = useQuery("destinations", () => 
     API.get("/destinations")
-  .then(res => res.data.destinations), {staleTime: Infinity})
+  .then(res => res.data), {staleTime: Infinity})
 
 
   if(destinations.isError){
       console.log(destinations.error)
       return null
   }
-
+console.log(destinations)
   return destinations.isLoading
   ?(
       <h3>Loading...</h3>
   )
   : (
       <ul>
-          {destinations?.data.map(resort => (
+          {/* {destinations?.data.map(resort => (
               <li key={resort.id} onClick={()=>setResort(resort)}>{resort.name}</li>
-          ))}
+          ))} */}
       </ul>
   )
 
