5e93afbe33f85953198ea23428dfd3c1291ce1e2
Chris Blagg <chrisblagg@chriss-mbp.lan>
2021-07-13 23:37:13 -0700

    proxy test

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'https://api.themeparks.wiki/v1',
+    baseURL: '/v1',
     headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
